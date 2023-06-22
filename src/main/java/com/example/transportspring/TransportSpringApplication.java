a0d365e8ec9f9e76a5a84947ef0eaa104747da64
Tomokou <bychckova.margo@yandex.ru>
2023-06-23 00:21:22 +0300

    Initial commit

---
+++ src/main/java/com/example/transportspring/TransportSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.transportspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/transportspring/TransportSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transportspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
